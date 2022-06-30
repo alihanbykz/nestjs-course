bc5161a257b981ca5135307a51887a78b74f5b7c
alihanbykz <alihan.buyukoz@ceng.deu.edu.tr>
2022-06-30 13:24:22 +0300

    all exception filter added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { AllExceptionFilter } from 'libs/filters/all-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost); 
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapter)); // Global Filter ekledik
+  app.setGlobalPrefix('api'); //localhost:3000/api/user
   await app.listen(3000);
 }
 bootstrap();
